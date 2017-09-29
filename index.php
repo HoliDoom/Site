52e18a713c9a80a7271745ff66b026ff8c526c7e
HoliDoom <32386197+HoliDoom@users.noreply.github.com>
2017-09-29 12:01:10 +0300

    My first site

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header>
+    <div id="logo"><a href="#">Блог-визитка-чат</a></div>
+    <nav>
+    	<ul>
+			<li><a href="#">Блог</a></li>
+    		<li><a href="#">Чат</a></li>
+ 		 </ul>
+    </nav>
+  </header>
+
+<main>
+	<h1>Устал быть аутсайдером?<br>Меняй себя уже  сегодня!</h1>
+	<p>"Блог-визитка успешного человека поможет тебя в этом."</p>
+	
+	<article class=" change">
+		
+		
+		<blockquote >Возьмите идею. Сделайте ее своей жизнью — думайте о ней, мечтайте о ней, живите ею. Пусть ваш разум, мышцы, нервы, каждая часть тела будет наполнена этой одной идеей. Вот он — путь к успеху. </blockquote>
+  <p>Swami Vivekananda</p>
+<!--      <h2>Чтобы добиться успеха- <br>нужно действовать </h2>
+      <p>"Та ну его! <br>
+      		Это же так сложно для меня!
+      		Это не мое!<br>
+      		Найду что-небудь попроще!<br>
+      		Пойду искать счастье в другом месте!"<br><br><br><br>
+      		А почему так думает большенство?
+      		</p>-->
+      		 </article>
+      		 
+      		 
+      		 
+      		 <form action="" class="reg">
+      		 	<label for="name" >Имя:</label><input type="text" name="name" id="UserName" placeholder="Ваше имя" ><br><br>
+      		 	<label for="soname" >Фамилия:</label><input type="text" name="soname" id="UserSoName" placeholder="Ваше фамилия" ><br><br>
+      		 	<label for="email" >E-mail:</label><input type="text" name="email" id="UserEmail" placeholder="Ваш e-mail"><br><br>
+      		 	<label for="phone"  >Телефон:<input type="text" name="phone" id="UserPhone" placeholder="Ваш телефон"></label><br><br>
+      		 	<button>Отправить</button>
+      		 </form>
+      		 
+      		 <div class="clear"></div>
+      		 
+      		<article class=" areyou">
+      	
+      <h2>А ты успешный? </h2>
+      <img src="img/About.jpg" alt="about" title="A ты успешный?">
+      <p>  О том, как стать успешным не говорит и не размышляет только самый ленивый (и, наверняка, неуспешный). На эту тему проводится множество семинаров, тренингов, встреч, люди строят теории, создают правила, выводят формулы успеха. Что-то из этого работает, а что-то нет, ведь волшебной пилюли не существует, а инъекцию успеха еще не придумали.Однако в процессе долгих наблюдений за человечеством удалось выявить ряд качеств, которые отличают успешного человека от неуспешного.       Что-то из этого работает, а что-то нет, ведь волшебной пилюли не существует, а инъекцию успеха еще не придумали.Однако в процессе долгих наблюдений за человечеством удалось выявить ряд качеств, которые отличают успешного человека от неуспешного.   		</p>
+      		
+    </article>
+    
+    <article class="price">
+    	<div>
+    		<h2>Just</h2>
+    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non tincidunt mi. Cras ullamcorper, eros in tempus placerat, tellus diam varius mauris, sollicitudin ultricies neque dui eget quam. Mauris lacus nunc, molestie at varius vel, tempus eget lacus. Pellentesque iaculis, lorem eu porttitor condimentum, magna ante condimentum nunc, a vulputate massa velit vitae erat.</p>
+    		<a href="#">Подписаться 99.99$</a>
+    	</div>
+    	
+    	<div>
+    		<h2>Inter-Price</h2>
+    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non tincidunt mi. Cras ullamcorper, eros in tempus placerat, tellus diam varius mauris, sollicitudin ultricies neque dui eget quam. Mauris lacus nunc, molestie at varius vel, tempus eget lacus. Pellentesque iaculis, lorem eu porttitor condimentum, magna ante condimentum nunc, a vulputate massa velit vitae erat.</p>
+    		<a href="#">Подписаться 199.99$</a>
+    	</div>
+    	
+    	<div> 
+    		<h2>Super-inter-price</h2>
+    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non tincidunt mi. Cras ullamcorper, eros in tempus placerat, tellus diam varius mauris, sollicitudin ultricies neque dui eget quam. Mauris lacus nunc, molestie at varius vel, tempus eget lacus. Pellentesque iaculis, lorem eu porttitor condimentum, magna ante condimentum nunc, a vulputate massa velit vitae erat.</p>
+    		<a href="#">Подписаться 599.99$</a>
+    	</div>
+    </article>
+    
+    <div class="clear"></div>
+    
+    
+</main>
+
+<footer>
+<a href="https://facebook.com" target="_blank"><img src="img/fb.png" alt="Our group in Facebook" title="Our group in Facebook"></a>
+<a href="https://twitter.com" target="_blank"><img src="img/tw.png" alt="Our group in Twitter"  title="Our group in Twitter"></a>
+<a href="https://vk.com" target="_blank"  title="Our group in VK"><img src="img/vk.png" alt="Our group in VK"></a >
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m0!4v1505920147344!6m8!1m7!1sUrN6JeX1OYdnuJO2MQr4yA!2m2!1d50.44087627938953!2d30.51987386191928!3f50.41420461141497!4f19.2531882126746!5f0.7820865974627469"  frameborder="0" style="border:0" allowfullscreen></iframe>
+</footer>
+	
+</body>
+</html>
